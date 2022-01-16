a529a443033306a96b0ed65710356d6ade670fad
Shadow Monarch <53879034+oneeyedreaper@users.noreply.github.com>
2022-01-17 02:28:56 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import Card from "./Components/Card";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Skills from "./Components/Skills";
 import ContactMe from "./Components/ContactMe";
+import aos from "aos";
+import "aos/dist/aos.css";
 function App() {
+	useEffect(() => {
+		aos.init({
+			once: true,
+		});
+	}, []);
 	return (
 		<div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-			<Card />
-			<About />
+			<div data-aos="fade-down" data-aos-duraion="800">
+				<Card />
+			</div>
+
+			<div data-aos="fade-up" data-aos-duraion="800" data-aos-delay="400">
+				<About />
+			</div>
+
 			<Skills />
 			<ContactMe />
 			<Footer />
--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -2,16 +2,15 @@ import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 function ContactMe() {
 	return (
-		<div className="max-w-lg  w-full text-center pt-36 pb-10 ">
-			<h1 className="text-4xl hover:tooltip">
-				{" "}
+		<div className="max-w-lg  w-full text-center pt-36 pb-10 m-auto ">
+			<h1 className="text-2xl md:text-4xl hover:tooltip">
 				Interested in Working Together?
 			</h1>
 			<a className="" href="mailto:paras.asa28@gmail.com">
-				<span className="mt-10 inline-block bg-black w-auto font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer  text-gray-800 hover:bg-gray-800 rounded-full hover:text-white tranis duration-300">
-					<div className="flex flex-row items-centersjajai">
-						<span className="mr-3">Get in Touch</span>
-						<FaArrowRight color="white" />
+				<span className="mt-6 inline-block bg-black w-auto font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer ">
+					<div className="flex flex-row  items-centers">
+						<span className="mr-3 ">Get in Touch</span>
+						<FaArrowRight color="white" className="m-auto" />
 					</div>
 				</span>
 			</a>
